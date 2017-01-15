46949d56aa62113d60050ed13f54ad6820791056
Yusuke Hosonuma <tobi462@gmail.com>
2017-01-15 10:58:47 +0900

    Add: regex functions

--- SwiftCommons/Regex.swift
+++ SwiftCommons/Regex.swift
@@ -8,16 +8,151 @@
 
 import Foundation
 
+public protocol RegexMatcher {
+    
+    /// Is match string or not.
+    ///
+    /// - Parameter target: target string
+    /// - Returns: true if any matches, otherwise false.
+    func isMatch(_ target: String) -> Bool
+    
+    /// Return first match.
+    ///
+    /// - Parameter target: target string
+    /// - Returns: `Match` if matched, otherwise nil.
+    func match(_ target: String) -> Match?
+    
+    /// Return all matches.
+    ///
+    /// - Parameter target: target string
+    /// - Returns: `Match` array if matched.
+    func matches(_ target: String) -> [Match]
+    
+    /// Replace string.
+    ///
+    /// - Parameters:
+    ///   - source: source string
+    ///   - to: replacement string
+    /// - Returns: replaced string.
+    func replace(in source: String, to: String) -> String
+}
+
+public func regex() -> RegexPatternBuilder {
+    return RegexPatternBuilder()
+}
+
+/// Create `Regex`, force unwarpped.
+/// This is syntax sugar, convenience if pattern is statically string.
+///
+/// - Parameter pattern: pattern
+/// - Returns: force unwapped `Regex`
+public func regex(_ pattern: String) -> RegexBuilder {
+    return RegexBuilder(pattern)
+}
+
+
+public struct RegexPatternBuilder {
+    func pattern(_ pattern: String) -> RegexBuilder {
+        return RegexBuilder(pattern)
+    }
+}
+
+public struct RegexBuilder {
+    
+    fileprivate let pattern: String
+    fileprivate var options: NSRegularExpression.Options = []
+    
+    init(_ pattern: String) {
+        self.pattern = pattern
+    }
+    
+    func caseInsensitive() -> RegexBuilder {
+        var builder = self
+        builder.options.insert(.caseInsensitive)
+        return builder
+    }
+    
+    func allowCommentsAndWhitespace() -> RegexBuilder {
+        var builder = self
+        builder.options.insert(.allowCommentsAndWhitespace)
+        return builder
+    }
+    
+    func ignoreMetacharacters() -> RegexBuilder {
+        var builder = self
+        builder.options.insert(.ignoreMetacharacters)
+        return builder
+    }
+    
+    func dotMatchesLineSeparators() -> RegexBuilder {
+        var builder = self
+        builder.options.insert(.dotMatchesLineSeparators)
+        return builder
+    }
+    
+    func anchorsMatchLines() -> RegexBuilder {
+        var builder = self
+        builder.options.insert(.anchorsMatchLines)
+        return builder
+    }
+    
+    func useUnixLineSeparators() -> RegexBuilder {
+        var builder = self
+        builder.options.insert(.useUnixLineSeparators)
+        return builder
+    }
+    
+    func useUnicodeWordBoundaries() -> RegexBuilder {
+        var builder = self
+        builder.options.insert(.useUnicodeWordBoundaries)
+        return builder
+    }
+}
+
+extension RegexBuilder: RegexMatcher {
+    
+    private var regex: Regex {
+        return Regex(self.pattern, options: self.options)!
+    }
+    
+    public func isMatch(_ target: String) -> Bool {
+        return self.regex.isMatch(target)
+    }
+    
+    public func match(_ target: String) -> Match? {
+        return self.regex.match(target)
+    }
+    
+    public func matches(_ target: String) -> [Match] {
+        return self.regex.matches(target)
+    }
+    
+    public func replace(in source: String, to: String) -> String {
+        return self.regex.replace(in: source, to: to)
+    }
+}
+
 public struct Regex {
     
-    private let regex: NSRegularExpression
+    fileprivate let regex: NSRegularExpression
     
-    public init?(_ pattern: String) {
+    public init?(_ pattern: String, options: NSRegularExpression.Options = []) {
         do {
-            self.regex = try NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+            self.regex = try NSRegularExpression(pattern: pattern, options: options)
         } catch {
             return nil
         }
+    }
+}
+
+extension Regex: RegexMatcher {
+
+    public func isMatch(_ target: String) -> Bool {
+        return self.matches(target).count > 0
+    }
+    
+    public func match(_ target: String) -> Match? {
+        return self.matches(target).first
     }
     
     public func matches(_ target: String) -> [Match] {
@@ -25,6 +160,11 @@ public struct Regex {
         return self.regex.matches(in: target, range: range).map {
             Match(target: target, result: $0)
         }
+    }
+    
+    public func replace(in source: String, to: String) -> String {
+        let range = NSRange(location: 0, length: source.characters.count)
+        return self.regex.stringByReplacingMatches(in: source, range: range, withTemplate: to)
     }
 }
 
